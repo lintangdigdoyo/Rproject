0180e3386227eb91fc7f778506c6739767737fba
lintangdigdoyo <38762222+lintangdigdoyo@users.noreply.github.com>
2018-10-07 20:50:13 +0700

    Update Differentiation.r

--- Differentiation.r
+++ Differentiation.r
@@ -7,33 +7,25 @@ rule11 <- function(x,n){
 
 
 library(Ryacas)
-
 x <- Sym("x")
-
 Simplify(deriv(x^2,x))
 
 
 
 rule8 <- function(x){
-
     return (sqrt(x))
-
 }
 
 
 
 library(Ryacas)
-
 x <- Sym("x")
-
 Simplify(deriv(sqrt(x),x))
 
 
 
 #Nomor 1
-
 #y = 2x^5
-
 
 
 D(expression(2*x^5), 'x')
@@ -43,9 +35,7 @@ D(expression(2*x^5), 'x')
 
 
 
-
 #Nomor 2
-
 #y = x^2+4
 
 
@@ -55,9 +45,7 @@ D(expression(x^2+4), 'x')
 
 
 #Nomor 3
-
 #y = x^5 - 6*x^7
-
 
 
 D(expression(x^5 - 6*x^7), 'x')
